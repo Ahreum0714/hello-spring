a523d33514b8acc1d6d7f20c7b1d546f38bdd78b
Ahreum0714 <yalin7145@gmail.com>
2023-06-01 10:48:01 +0900

    AOP - AOP 적용

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +31,11 @@ public class SpringConfig {
         return new MemberService(memberRepository);
     }
 
+/*    @Bean //SpringConfig에서 직접 빈 등록하거나, TimeTraceAop클래스에 @Component 해주거나!
+    public TimeTraceAop timeTraceAop() {
+        return new TimeTraceAop();
+    }*/
+    
 //    @Bean
 //    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -27,18 +27,9 @@ public class MemberService {
     * 회원가입
     * */
     public Long join(Member member) {
-
-        long start = System.currentTimeMillis();
-
-        try{
-            validateDuplicateMember(member); // 중복 회원 검증
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = " + timeMs + "ms");
-        }
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -52,26 +43,10 @@ public class MemberService {
     * 전체 회원 조회
     */
     public List<Member> findMembers() {
-
-        long start = System.currentTimeMillis();
-        try {
-            return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers = " + timeMs + "ms");
-        }
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
-
-        long start = System.currentTimeMillis();
-        try {
-            return memberRepository.findById(memberId);
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findOne = " + timeMs + "ms");
-        }
+        return memberRepository.findById(memberId);
     }
 }
